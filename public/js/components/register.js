1678d126d13d1d40024364fdffadab637e8cac57
liyanyan19940914 <921258351@qq.com>
2017-04-08 18:06:45 +0800

    modify css

--- public/js/components/register.js
+++ public/js/components/register.js
@@ -1,32 +1,32 @@
-import React,{Component} from "react";
+import React, {Component} from "react";
 import Header from "../containers/header"
 
-class Register extends  Component{
-    render(){
+class Register extends Component {
+    render() {
         return <div>
             <Header/>
             <div className="backgroundImg">
-        <div className="main">
-            <center>
-            <div className="border">
-            <h3 className="headline">注册</h3>
-            <div>
-                <input type="text"  className="inputStyle" id="userName" placeholder="请输入用户名"/>
-            </div>
-            <div>
-                <input type="password" className="inputStyle" id="password" placeholder="请输入密码"/>
-            </div>
-            <div>
-                <input type="password" className="inputStyle" id="confirm" placeholder="请再次输入密码"/>
-            </div>
-            <div>
-                <button type="submit" className="buttonType">注册</button>
-            </div>
-             </div>
-            </center>
-        </div>
+                <div className="main">
+                    <center>
+                        <div className="border">
+                            <h3 className="headline">注册</h3>
+                            <div>
+                                <input type="text" className="inputStyle" id="userName" placeholder="请输入用户名"/>
+                            </div>
+                            <div>
+                                <input type="password" className="inputStyle" id="password" placeholder="请输入密码"/>
+                            </div>
+                            <div>
+                                <input type="password" className="inputStyle" id="confirm" placeholder="请再次输入密码"/>
+                            </div>
+                            <div>
+                                <button type="submit" className="buttonType">注册</button>
+                            </div>
+                        </div>
+                    </center>
                 </div>
             </div>
+        </div>
     }
 
 }
