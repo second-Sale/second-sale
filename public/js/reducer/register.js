0de9dd17891501983a92c503dda9b98c7b5e37e1
liyanyan19940914 <921258351@qq.com>
2017-04-25 15:12:52 +0800

    container can receive date

--- public/js/reducer/register.js
+++ public/js/reducer/register.js
@@ -1,6 +1,9 @@
-module.exports = ((state={},action)=>{
+module.exports = ((state={isExitUser:false},action)=>{
     switch(action.type){
-        case 'REGISTER_COMMIT':alert(action.data);break;
+        case 'REGISTER_COMMIT':{
+            console.log("reducer",action.isExitUser);
+            return({isExitUser:action.isExitUser});
+        }
         default: return state;
     }
 })
