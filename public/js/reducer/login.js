4a0634b8527da95a9470617c16cc146d72ca25c4
liyanyan19940914 <921258351@qq.com>
2017-05-02 21:11:20 +0800

    reduce can receive middleware data

--- public/js/reducer/login.js
+++ public/js/reducer/login.js
@@ -1,3 +1,9 @@
-/**
- * Created by liyan on 17-5-2.
- */
+module.exports = ((state={judge:""},action)=>{
+    switch(action.type){
+        case 'LOGIN':{
+            console.log("reducer",action.judge);
+            return({judge:action.judge});
+        }
+        default: return state;
+    }
+});
