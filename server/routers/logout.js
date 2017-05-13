113bf93808763b0bc0b094a4db0e8a2c41d7a0d0
liyanyan19940914 <921258351@qq.com>
2017-05-13 16:05:02 +0800

    modify logout

--- server/routers/logout.js
+++ server/routers/logout.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/logout',(req,res)=>{
-    req.session.loginUser = '';
-    res.send({isTrue:'0',user:req.session.loginUser});
+    req.session.loginUser = null;
+    res.send({user:req.session.loginUser});
 })
 
 module.exports = router;
