a0a34b47a4e3e9e48875e59b5eb40dccc6ee82f0
liyanyan19940914 <921258351@qq.com>
2017-06-03 15:12:09 +0800

    image can show view

--- public/js/components/imageUplond.js
+++ public/js/components/imageUplond.js
@@ -2,22 +2,43 @@ import React,{Component} from 'react';
 import request from 'superagent';
 
 class Image extends Component{
-    handleClick() {
-        let file = this.refs.file.files[0];
-        let formData = new FormData();
-        formData.append('avatar', file);
-        request.post('/profile')
+
+    isClick(){
+        let  input=this.refs.file;
+        let  img=this.refs.image;
+        if(input.files&&input.files[0]){
+            var fileObj=new FileReader();
+            fileObj.onload=function(event){//属性绑定方法
+                img.src=event.target.result;
+
+            };
+            fileObj.readAsDataURL(input.files[0]);
+        }
+        this.handleClick(input.files[0]);
+    }
+    handleClick(file){
+        console.log(this.props);
+        let formData=new FormData();
+        formData.append('avatar',file);
+        request
+            .post('/profile')
             .send(formData)
-            .end((err, res)=> {
-                console.log(res.body);
-            })
+            .end((err,res)=>{
+                console.log(res.body.filePath);
+                //this.props.uploadImage(res.body.filePath);
+            });
     }
 
     render() {
         return (
             <div>
-                <input type="file" ref="file"/>
-                <button onClick={this.handleClick.bind(this)}>上传</button>
+                <div>
+                    <label className="location">照片：</label>
+                    <input type="file" ref="file" className="fileStyle" onChange={this.isClick.bind(this)}/>
+                </div>
+                <div>
+                    <img src="" className="imgType" ref="image"/>
+                </div>
             </div>
         )
     }
